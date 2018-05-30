d0bdbcf1d32c7c5d863e75e7d4f5d35ce8876b0a
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-30 11:16:01 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,7 @@ def create_project_hash
   #image link: project.css("div.project-thumbnail a img").attribute("src").value
   #description: project.css(p.bbcard_blurb").text 
   #location: project.css(".location-name")
+  #percent_funded: project.css("ul.project-stats li.first.funded strong").text
   
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
