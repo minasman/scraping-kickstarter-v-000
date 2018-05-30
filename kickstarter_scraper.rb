78005a07feef877b9ce5ba36dbba9f3657bf1551
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-30 10:58:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
+  #projects: kickstarter.css("li.project.grid_4")
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   binding.pry 
